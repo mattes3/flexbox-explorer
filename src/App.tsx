39fb1003eaf48a9d21e12838d3ec8c9eca0e0233
Matthias Bohlen <mbohlen@mbohlen.de>
2020-05-17 10:08:14 +0200

    First try.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { rowStyles, columnStyles } from './CellAlignment';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const top = { backgroundColor: '#ddd', height: '300px', ...columnStyles('top') };
+    const middle = { backgroundColor: '#fff', height: '300px', ...columnStyles('middle') };
+    const bottom = { backgroundColor: '#ddd', height: '300px', ...columnStyles('bottom') };
+    const left = { backgroundColor: '#fbb', ...rowStyles('left') };
+    const center = { backgroundColor: '#bfb', ...rowStyles('center') };
+    const right = { backgroundColor: '#bbf', ...rowStyles('right') };
+    return (
+        <div>
+            <div style={top}>
+                <div style={left}>
+                    <p>top left</p>
+                </div>
+                <div style={center}>
+                    <p>top center</p>
+                </div>
+                <div style={right}>
+                    <p>top right</p>
+                </div>
+            </div>
+            <div style={middle}>
+                <div style={left}>
+                    <p>middle left</p>
+                </div>
+                <div style={center}>
+                    <p>middle center</p>
+                </div>
+                <div style={right}>
+                    <p>middle right</p>
+                </div>
+            </div>
+            <div style={bottom}>
+                <div style={left}>
+                    <p>bottom left</p>
+                </div>
+                <div style={center}>
+                    <p>bottom center</p>
+                </div>
+                <div style={right}>
+                    <p>bottom right</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
